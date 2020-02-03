edabfa4f663c49915f4462e717f911e76e01bfe8
MohamedA-M-Hassan <anehmr0@gmail.com>
2020-02-03 14:21:24 +0200

    models: add customer and deal models

--- deal_app/admin.py
+++ deal_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from deal_app.models import Customer,Deal
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Deal)
--- deal_app/models.py
+++ deal_app/models.py
@@ -3,6 +3,40 @@ from django.db import models
 # Create your models here.
 
 
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    appreviation = models.CharField(max_length=15)
+    country = models.CharField(max_length=30)
+    region = models.CharField(max_length=30)
+    lob = models.CharField(max_length = 30)
+    def __str__(self):
+        return self.name
+
+
+
+class Deal(models.Model):
+
+    name = models.CharField(max_length = 20)
+    creation_date =models.DateTimeField()
+    date_lastupdated = models.DateField(auto_now=True)
+    status = models.CharField(max_length = 20) ##in progress or win or loss
+    status_descriptian = models.TextField()
+    deal_source = models.CharField(max_length=50)
+    third_party = models.CharField(max_length=50)
+    project= models.CharField(max_length=50)
+    margin_type = models.CharField(max_length=50)
+    new_renew = models.CharField(max_length=10)
+    sub_perpcual = models.CharField(max_length=50)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+    def __str__(self):
+        return self.name
+
+
+
+
+
+### add primary key: id = models.AutoField(primary_key=True)
+'''
 class Topic(models.Model):
     top_name = models.CharField(max_length=264,unique= True)
 
@@ -23,4 +57,5 @@ class  AccessRecrd(models.Model):
     date = models.DateField()
     def __str__(self):
         return str(self.date)
-        
+
+'''
