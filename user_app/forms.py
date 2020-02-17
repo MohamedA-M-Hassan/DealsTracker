2fb0a6d83dade4c18bcae508f6e847fa642703db
MohamedA-M-Hassan <anehmr0@gmail.com>
2020-02-17 13:03:49 +0200

    userApp: add Sign up

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from user_app.models import User
 
 
-class User_Form(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget = forms.Textarea)
+class User_Form(forms.ModelForm):
+
+#    name = forms.CharField()
+#    email = forms.EmailField()
+#    password = forms.CharField()
+#    phone = forms.CharField()
+
+    class Meta():
+        model = User
+        fields = '__all__'
+
+    #text = forms.CharField(widget = forms.Textarea)
--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from . import forms
+#from . import forms
+from user_app.forms import User_Form
 
 # Create your views here.
 
@@ -7,5 +8,16 @@ def index(request):
     return render(request,'user_app/index.html')
 
 def sign_up(request):
-    form = forms.User_Form()
+
+
+    #form = forms.User_Form()
+    form = User_Form()
+    if request.method == 'POST':
+        form = User_Form(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return index(request)
+        else:
+            print('error in view.py user_app')
     return render(request, 'user_app/sign_up.html', {'form':form})
