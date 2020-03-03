22f3db1a29b25948fdd9515461e1de4d75ae07b0
MohamedA-M-Hassan <anehmr0@gmail.com>
2020-03-03 15:53:44 +0200

    add deal items and it's price

--- deal_app/admin.py
+++ deal_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from deal_app.models import Customer,Deal
+from deal_app.models import Customer,Deal,Deal_Items_Price_Lookup,Deal_Items
 
 # Register your models here.
 
 admin.site.register(Customer)
 admin.site.register(Deal)
+admin.site.register(Deal_Items)
+admin.site.register(Deal_Items_Price_Lookup)
--- deal_app/models.py
+++ deal_app/models.py
@@ -13,13 +13,19 @@ class Customer(models.Model):
         return self.name
 
 
-
+## Customer - Ejada info - Summary file: EjadacColumns=e_ , CustomerColumns= c_, generaLColumns= g_
 class Deal(models.Model):
 
+    ## general columns
     name = models.CharField(max_length = 20)
+    ## one to many relation
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
     creation_date =models.DateField()
     date_lastupdated = models.DateField(auto_now=True)
     status = models.CharField(max_length = 20) ##in progress or win or loss
+
+    ## Ejada columns to be used in summary file
     status_descriptian = models.TextField()
     deal_source = models.CharField(max_length=50)
     third_party = models.CharField(max_length=50)
@@ -27,35 +33,63 @@ class Deal(models.Model):
     margin_type = models.CharField(max_length=50)
     new_renew = models.CharField(max_length=10)
     sub_perpcual = models.CharField(max_length=50)
-    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+
+
+    ## Customer fields to be added by himself
+
+
     def __str__(self):
         return self.name
 
 
-
-
-
-### add primary key: id = models.AutoField(primary_key=True)
-'''
-class Topic(models.Model):
-    top_name = models.CharField(max_length=264,unique= True)
-
-    def __str__(self):
-        return self.top_name
-
-
-class Webpage(models.Model):
-    topic = models.ForeignKey(Topic)
-    name = models.CharField(max_length=264,unique= True)
-    url = models.URLField(unique=True)
+class Deal_Items_Price_Lookup(models.Model):
+    creator_on_prem_price_per_month_dollar = 70
+    creator_on_prem_price_per_month_dollar = 0
+    explorer_on_prem_price_per_month_dollar = 0
+    viewer_on_prem_price_per_month_dollar = 0
+    term_desktop_personal_price_per_month_dollar = 0
+    term_desktop_professional_price_per_month_dollar = 0
+    term_server_core_price_per_month_dollar = 0
+    term_server_interactor_price_per_month_dollar = 0
+    tableau_online_price_per_month_dollar = 0
+    server_management_price_per_month_dollar = 0
+    creator_online_price_per_month_dollar = 0
+    explorer_online_price_per_month_dollar = 0
+    viewer_online_price_per_month_dollar = 0
+    perpetual_desktop_personal_price_per_month_dollar = 0
+    perpetual_desktop_professinal_price_per_month_dollar = 0
+    perpetual_server_core_price_per_month_dollar = 0
+    perpetual_server_interactor_price_per_month_dollar = 0
+    data_management_online_price_per_month_dollar = 0
+    data_management_on_premise_price_per_month_dollar = 0
     def __str__(self):
         return self.name
 
+class Deal_Items(models.Model):
+    name = models.CharField(max_length = 20)
+    ## one to many relation
+    deal = models.ForeignKey(Deal,on_delete = models.CASCADE)
+    ## one to one relation
+    deal_price = models.OneToOneField(Deal_Items_Price_Lookup,on_delete= models.CASCADE)
 
-class  AccessRecrd(models.Model):
-    name = models.ForeignKey(Webpage,on_delete=models.PROTECT)
-    date = models.DateField()
+    creator_on_prem = models.BooleanField(default= False)
+    explorer_on_prem = models.BooleanField(default= False)
+    viewer_on_prem = models.BooleanField(default= False)
+    term_desktop_personal = models.BooleanField(default= False)
+    term_desktop_professional = models.BooleanField(default= False)
+    term_server_core = models.BooleanField(default= False)
+    term_server_interactor = models.BooleanField(default= False)
+    tableau_online = models.BooleanField(default= False)
+    server_management = models.BooleanField(default= False)
+    creator_online = models.BooleanField(default= False)
+    explorer_online = models.BooleanField(default= False)
+    viewer_online = models.BooleanField(default= False)
+    perpetual_desktop_personal = models.BooleanField(default= False)
+    perpetual_desktop_professinal = models.BooleanField(default= False)
+    perpetual_server_core = models.BooleanField(default= False)
+    perpetual_server_interactor = models.BooleanField(default= False)
+    data_management_online = models.BooleanField(default= False)
+    data_management_on_premise = models.BooleanField(default= False)
     def __str__(self):
-        return str(self.date)
-
-'''
+        return self.name
